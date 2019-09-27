56d4981fdb3cfa3784d030640cb32e987a74f8fe
EZHoon93 <47181408+EZHoon93@users.noreply.github.com>
2019-09-27 22:35:50 +0900

    2장 매출계산 프로그램
    및 프로젝트 복구

---
+++ Csharp_Practical/Csharp_Practical/Ch2/Ch2-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SalesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sales = new SalesCounter("sales.csv");
+            //아래와 같이 new로 해서하지않는다. 초보가 하는실수.
+            //Dictionary<string, int> amountPerStore = new Dictionary<string, int>();
+            //amountPerStore = sales.GetPerStoreSales();
+            var amountPerStore = sales.GetPerStoreSales();
+            //foreach (KeyValuePair<string, int> obj in amountPerStore)
+            foreach (var obj in amountPerStore)
+            {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ Csharp_Practical/Csharp_Practical/Ch2/Ch2-2/SalesCounter.cs
@@ -0,0 +1,73 @@
+﻿// List 2-22
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SalesCalculator {
+    // 매출 계산 클래스
+    public class SalesCounter
+    {
+        //세일 클래스를 리스트 생성, 
+        //private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
+
+
+        //생성자
+        public SalesCounter(string filePath)
+        {
+            _sales = ReadSales(filePath);
+        }
+
+        //점포별 매출을 구한다
+        //public Dictionary<string, int> GetPerStoreSales()
+        public IDictionary<string,int> GetPerStoreSales()
+        {
+            //Dictionary<string, int> dict = new Dictionary<string, int>();
+            //Var로 바꾼다.
+            var dict = new Dictionary<string, int>();
+
+            foreach (var sale in _sales)
+            {
+                //딕셔너리에 이미있으면 더한다
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                //없으면 초기값을 설정해준다
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+
+        //매출 데이터를 읽어들인다, Sale 오브젝트의 리스트를 반환, 생성자에서 호출한다
+        //private static List<Sale> ReadSales(string filePath)
+        private static IEnumerable<Sale> ReadSales(string filePath)
+
+        {
+            var sales = new List<Sale>();
+            //걍로에있는 파일을 전부읽어온다
+            var lines = File.ReadAllLines(filePath);
+            //모든줄을 한줄씩 읽는다
+            foreach(var line in lines)
+            {
+                //토큰 ,로 나눈다
+                var items = line.Split(',');
+                //객체 이니셜라이즈 사용. 
+                /* Sale sale = new Sale()
+                 * sale.ShopName = items[0]; 
+                 * 이렇게 사용X , 가독성을 떨어트린다.
+                 * */
+                var sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
+    }
+}
